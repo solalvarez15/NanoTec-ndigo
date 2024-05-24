9a886e12ce4d83b0b23d575fdf902905e6670061
rosariomolinas <130401216+rosariomolinas@users.noreply.github.com>
2024-05-23 22:27:21 -0300

    Delete src directory

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const { Router: expressRouter } = require("express");
-const router = expressRouter();
-// auth routes
-const pacRouter = require("./pacRoute");
-router.use("/pacientes", pacRouter);
-module.exports = router;
